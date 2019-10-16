ce057f604d403e951cbb956c4fb8a36448bc76d5
wayne <wayne@gmail.com>
2019-10-16 20:50:24 +0800

    updated

--- js/javascript.js
+++ js/javascript.js
@@ -53,3 +53,17 @@ function newElement() {
     }
   }
 }
+
+
+
+
+var width = 10;
+function move() {
+      var elem = document.getElementById("myBar");
+        if (width < 100) {
+          width+=2;
+          elem.style.width = width + '%';
+          elem.innerHTML = width * 1 + '%';
+        }
+
+}
