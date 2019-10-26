a21a7caf700984d90da79faf0728f1712795f61f
wayne <wayne@gmail.com>
2019-10-26 18:38:05 +0800

    updateedddd

--- js/javascript.js
+++ js/javascript.js
@@ -9,6 +9,17 @@ for (i = 0; i < myNodelist.length; i++) {
   myNodelist[i].appendChild(span);
 }
 
+// Create a "decrease" button and append it to each list item
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "opening";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
 // Click on a close button to hide the current list item
 var close = document.getElementsByClassName("close");
 var i;
@@ -29,44 +40,41 @@ list.addEventListener('click', function(ev) {
 
 // Create a new list item when clicking on the "Add" button
 function newElement() {
-  var li = document.createElement("li");
-  var inputValue = document.getElementById("myInput").value;
-  var t = document.createTextNode(inputValue);
-  li.appendChild(t);
-  if (inputValue === '') {
-    alert("You must write something!");
-  } else {
-    document.getElementById("myUL").appendChild(li);
-  }
-  document.getElementById("myInput").value = "";
-
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
-
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("myInput").value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue === '') {
+      alert("You must write something!");
+    } else {
+      document.getElementById("myUL").appendChild(li);
     }
+    document.getElementById("myInput").value = "";
+
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+
+    for (i = 0; i < close.length; i++) {
+      close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+      }
+    }
+}
+
+  // ================================= HEALTH BAR ================== //
+  var width = 10;
+  function move() {
+        var elem = document.getElementById("myBar");
+          if (width < 100) {
+            width+=2;
+            elem.style.width = width + '%';
+            elem.innerHTML = width * 1 + '%';
+          }
   }
-}
-
-
-
-
-var width = 10;
-function move() {
-      var elem = document.getElementById("myBar");
-        if (width < 100) {
-          width+=2;
-          elem.style.width = width + '%';
-          elem.innerHTML = width * 1 + '%';
-        }
-
-}
 
 
 function myFunction() {
@@ -76,4 +84,28 @@ function myFunction() {
   } else {
     x.className = "topnav";
   }
+}
+
+
+
+
+
+
+
+document.addEventListener( 'DOMContentLoaded',function(){
+ var searchpara=document.getElementById("paragraph").innerHTML;
+ searchpara=searchpara.toString();
+ document.getElementById("search").onclick =function ()
+ {highlight_word(searchpara)};
+},false);
+
+function highlight_word(searchpara)
+{
+ var text=document.getElementById("search_text").value;
+ if(text)
+ {
+  var pattern=new RegExp("("+text+")", "gi");
+  var new_text=searchpara.replace(pattern, "<span class='highlight'>"+text+"</span>");
+  document.getElementById("paragraph").innerHTML=new_text;
+ }
 }
